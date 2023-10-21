8c17abe353b828d7ab96863dde9ed3a0e077056e
Roman Gnativ <gnativromaneducation@gmail.com>
2023-10-21 09:38:46 +0500

    New Progect

---
+++ m16_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16_1
+//
+//  Created by Гнатив Роман on 21.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
