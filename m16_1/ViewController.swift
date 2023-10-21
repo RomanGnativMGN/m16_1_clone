29b6d072371a11a3443a2507c454e86a2fae7638
Roman Gnativ <gnativromaneducation@gmail.com>
2023-10-21 10:30:43 +0500

    Цвет встроился в проект

--- m16_1/ViewController.swift
+++ m16_1/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .darkGray
+        
         // первый комментарий
         
         // второй комментарий
